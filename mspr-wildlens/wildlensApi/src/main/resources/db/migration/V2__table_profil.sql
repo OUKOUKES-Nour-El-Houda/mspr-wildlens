550f5244fcf6e0bc2f90ce38f8eb6938450c7c72
OUKOUKES-Nour-El-Houda <nourelhoudaoukoukes@gmail.com>
2025-05-28 20:00:44 +0200

    Fix mspr-wildlens
    Former-commit-id: 694c120ee52f00b498aebde721494c4bd37b3bb0

--- mspr-wildlens/wildlensApi/src/main/resources/db/migration/V2__table_profil.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE profil (
-                        id BIGSERIAL PRIMARY KEY,          -- Clé primaire auto-incrémentée
-                        name_user VARCHAR(255) NOT NULL,    -- Nom de l'utilisateur
-                        username_user VARCHAR(255) UNIQUE NOT NULL,  -- Nom d'utilisateur unique
-                        mail_user VARCHAR(255) UNIQUE NOT NULL,     -- Adresse email unique
-                        password VARCHAR(255) NOT NULL,     -- Mot de passe (à stocker en format sécurisé)
-                        registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
--- mspr-wildlens/wildlensApi/src/main/resources/db/migration/V5__table_identification.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE identification (
-                                id BIGSERIAL PRIMARY KEY,                 -- Identifiant unique auto-incrémenté
-                                image_id BIGINT,                          -- Référence à l'image
-                                species_id BIGINT,                        -- Référence à l'espèce
-                                identification_date TIMESTAMP NOT NULL,   -- Date d'identification
-                                CONSTRAINT fk_image FOREIGN KEY (image_id) REFERENCES image(id),       -- Clé étrangère vers la table `image`
-                                CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES info_species(id) -- Clé étrangère vers la table `info_species`
-);
